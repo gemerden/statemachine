8f627a0e7e4409280cf0c79cf59efa2b68d7fff8
gemerden <gemerden@gmail.com>
2016-09-10 01:10:22 +0200

    moved example code to examples/basic.py

--- statemachine/bases.py
+++ statemachine/bases.py
@@ -1,7 +1,8 @@
 from functools import partial
 
-from statemachine.machine import TransitionError, StateMachine, MachineError
-from statemachine.tools import Path, has_doubles
+from statemachine.tools import Path
+
+__author__  = "lars van gemerden"
 
 
 class StateObject(object):
@@ -52,59 +53,4 @@ class StateObject(object):
 
     state = property(get_state, set_state)  # turn state into a property
 
-
-if __name__ == "__main__":
-    """
-    Small usage example: a minimal state machine (see also the tests)
-    """
-
-
-    def printer(action):
-        def func(obj):
-            print "'%s' for '%s' results in transition to %s" % (action, str(obj), obj.state)
-
-        return func
-
-
-    class LightSwitch(StateObject):
-
-        machine = StateMachine(
-            name="matter machine",
-            states=[
-                {"name": "on", "on_exit": printer("turn off"), "on_entry": printer("turn on")},
-                {"name": "off", "on_exit": printer("turn on"), "on_entry": printer("turn off")},
-            ],
-            transitions=[
-                {"old_state": "off", "new_state": "on", "triggers": ["turn_on", "switch"]},
-                {"old_state": "on", "new_state": "off", "triggers": ["turn_off", "switch"]},
-            ],
-        )
-
-        def __init__(self, name, initial="off"):
-            super(LightSwitch, self).__init__(initial=initial)
-            self.name = name
-
-        def __str__(self):
-            return self.name + " (%s)" % self.state
-
-
-    light_switch = LightSwitch("lights")
-
-    print light_switch.turn_on()
-    print light_switch, light_switch._state
-    light_switch.turn_off()
-    try:
-        light_switch.turn_off()
-    except TransitionError as e:
-        print "error: " + e.message
-    print
-    light_switch.switch()
-    light_switch.switch()
-    print
-    light_switch.state = "on"
-    light_switch.state = "off"
-    try:
-        light_switch.state = "off"  # does not result in any callbacks because the switch is already off
-    except TransitionError as e:
-        print e.message
 
