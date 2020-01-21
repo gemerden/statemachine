da0bbbbc94ead5565f7ea1d101f24ba6dccaf14b
gemerden <gemerden@gmail.com>
2020-01-21 17:10:25 +0100

    made importable from states directly

---
+++ states/__init__.py
@@ -0,0 +1,7 @@
+from states.machine import state_machine, StatefulObject
+from states.machine import TransitionError, MachineError
+
+__all__ = ("state_machine",
+           "StatefulObject",
+           "TransitionError",
+           "MachineError")
