5889fe6164c7b7d85c76c5fffd01f16d3916a9d3
gemerden <gemerden@gmail.com>
2020-05-18 18:22:02 +0200

    Update __init__.py

--- states/__init__.py
+++ states/__init__.py
@@ -1,7 +1,8 @@
 from states.machine import state_machine, StatefulObject
-from states.machine import TransitionError, MachineError
+from states.machine import TransitionError, MachineError, MultiStateObject
 
 __all__ = ("state_machine",
            "StatefulObject",
+           "MultiStateObject",
            "TransitionError",
            "MachineError")
