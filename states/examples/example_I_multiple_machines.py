fea6326442f03dd0d3df69e56181518df75affb3
gemerden <gemerden@gmail.com>
2020-12-18 10:33:58 +0100

    fix example

--- states/examples/example_I_multiple_machines.py
+++ states/examples/example_I_multiple_machines.py
@@ -1,102 +1,116 @@
-from states.machine import state_machine
+from states import state_machine
 from states import StatefulObject
 
 
 class Room(StatefulObject):
 
-    machine = state_machine(
-        states=[
-            {"name": "empty", "on_entry": ["write_left", "turn_off_lights"], "condition": "is_empty"},
-            {"name": "occupied", "on_entry": ["write_entered", "turn_on_lights"]},
-        ],
+    state = state_machine(
+        states={
+            "empty": {"on_entry": ["turn_off_lights", "write_empty"]},
+            "occupied": {"on_entry": ["turn_on_lights", "write_occupied"],
+                         "on_stay": "write_occupied"},
+        },
         transitions=[
-            {"old_state": "*", "new_state": "occupied", "triggers": "enter"},
-            {"old_state": "occupied", "new_state": "empty", "triggers": "exit"},
+            {"old_state": "*", "new_state": "occupied", "trigger": "enter", "on_transfer": "increment"},
+            {"old_state": "occupied", "new_state": {"empty": {"condition": "becomes_empty"},
+                                                    "occupied":{}},
+             "trigger": "leave"},
         ]
     )
 
-    def __init__(self, name, lights=None):
+    def __init__(self, name, lights=()):
         super(Room, self).__init__()
         self.name = name
-        self.lights = lights or []
-        self.people = 0
+        self.lights = lights
+        self.people = set()
 
-    def turn_on_lights(self, **kwargs):
+    def turn_on_lights(self, person, **kwargs):
         for light in self.lights:
-            if light.state == "off":
-                light.turn_on(room=self)
+            light.turn_on(room=self, person=person)
 
-    def turn_off_lights(self, **kwargs):
+    def turn_off_lights(self, person, **kwargs):
         for light in self.lights:
-            if light.state == "on":
-                light.turn_off(room=self)
+            light.turn_off(room=self, person=person)
 
-    def enter(self, person, **kwargs):
-        self.people += 1
-        if person.room:
-            person.room.exit(person=person)
-        person.room = self
-        self.machine.trigger(self, "enter", person=person, **kwargs)
+    def increment(self, person, **kwargs):
+        self.people.add(person)
 
-    def exit(self, **kwargs):
-        self.people -= 1
-        self.machine.trigger(self, "exit", **kwargs)
+    def becomes_empty(self, person, **kwargs):
+        self.people.remove(person)
+        return len(self.people) == 0
 
-    def is_empty(self, **kwargs):
-        return self.people == 0
+    def write_empty(self, **kwargs):
+        print(f"The {self.name} is now empty")
 
-    def write_left(self, **kwargs):
-        print("The %s is now empty" % self.name)
-
-    def write_entered(self, person, **kwargs):
-        print("%s just entered the %s" % (person.name, self.name))
+    def write_occupied(self, person, **kwargs):
+        print(f"{' and '.join(p.name for p in self.people)} {'is' if len(self.people) < 2 else 'are'} in the {self.name}")
 
 
-class Person(object):
+class Person(StatefulObject):
+
+    state = state_machine(
+        states={
+            "outside": {"on_entry": "leave_room"},
+            "inside": {"on_entry": "enter_room"},
+        },
+        transitions=[
+            {"old_state": "outside", "new_state": "inside", "trigger": "enter"},
+            {"old_state": "inside", "new_state": "outside", "trigger": "leave"},
+        ]
+    )
 
     def __init__(self, name):
         super(Person, self).__init__()
         self.name = name
         self.room = None
+        
+    def enter_room(self, room):
+        self.room = room
+        room.enter(person=self)
+
+    def leave_room(self, room):
+        self.room = None
+        room.leave(person=self)
 
 
 class Light(StatefulObject):
 
-    machine = state_machine(
-        states=[
-            {"name": "off", "on_entry": "write"},
-            {"name": "on", "on_entry": "write"},
-        ],
+    state = state_machine(
+        states={
+            "off": {"on_entry": "write"},
+            "on": {"on_entry": "write"},
+        },
         transitions=[
-            {"old_state": "off", "new_state": "on", "triggers": "turn_on"},
-            {"old_state": "on", "new_state": "off", "triggers": "turn_off"},
+            {"old_state": "off", "new_state": "on", "trigger": "turn_on"},
+            {"old_state": "on", "new_state": "off", "trigger": "turn_off"},
         ],
     )
 
     def __init__(self, name):
-        super(Light, self).__init__()
+        super().__init__()
         self.name = name
 
-    def write(self, room, **kwargs):
-        print("{light} in {room} turned {state}".format(light=self.name,
-                                                        room=room.name,
-                                                        state=self.state))
+    def write(self, room, person, **kwargs):
+        print(f"{person.name} turned the {self.name} in {room.name} {self.state}")
 
 
 if __name__ == "__main__":
 
     living = Room("living room", lights=[Light("ceiling lamp")])
-    bedroom = Room("bed room", lights=[Light("bed lamp")])
+    bedroom = Room("bedroom", lights=[Light("bed lamp")])
     kitchen = Room("kitchen", lights=[Light("sink light"), Light("ceiling lamp")])
 
     Ann = Person("Ann")
     Bob = Person("Bob")
 
-    living.enter(person=Ann)
-    living.enter(person=Bob)
-    kitchen.enter(person=Ann)
-    bedroom.enter(person=Bob)
-    bedroom.enter(person=Ann)
+    Ann.enter(room=living)
+    Bob.enter(room=living)
+    Ann.leave(room=living)
+    Ann.enter(room=kitchen)
+    Bob.leave(room=living)
+    Ann.leave(room=kitchen)
+    Bob.enter(room=bedroom)
+    Ann.enter(room=bedroom)
 
 
 
